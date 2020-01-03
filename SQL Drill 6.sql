a5554a050146a5ae1d8b6e847d1e658b2747abb9
Tori Jesser <tori.jesser@gmail.com>
2020-01-03 11:24:33 -0800

    update drill 6

--- SQL Drill 6.sql
+++ SQL Drill 6.sql
@@ -3,6 +3,7 @@ EXEC [dbo].[createZooDB]
 SELECT
 		a1.specialist_fname, a1.specialist_lname, a1.specialist_contact, a4.species_name
 		FROM tbl_specialist a1
-		FULL OUTER JOIN tbl_species a4 ON a4.species_id = a1.specialist_id
+		INNER JOIN tbl_care care ON care.care_specialist = a1.specialist_id
+		INNER JOIN tbl_species a4 ON a4.species_care = care.care_id
 		WHERE species_name = 'penguin'
 	;
